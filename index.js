ab46723a88e4e0919d1bd04af02f8dd931b30ed3
yi ge <dumaocn@gmail.com>
2025-06-06 23:53:05 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.TELEGRAM_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, '你好，我是 Render 部署的机器人！');
+});
